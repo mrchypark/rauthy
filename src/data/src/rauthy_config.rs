88be4712e06b2f43d24955078c2d896ad9ba7f3a
Luna Seillery <lseillery@bcasmart.com>
2026-05-06 16:00:52 +0200

    Add configurable HOTP length and frontend template.

--- src/data/src/rauthy_config.rs
+++ src/data/src/rauthy_config.rs
@@ -528,6 +528,7 @@ impl Default for Vars {
             },
             hmac_otp: VarsHOTP {
                 enable: false,
+                length: 6,
             },
             http_client: VarsHttpClient {
                 connect_timeout: 10,
@@ -2665,6 +2666,9 @@ impl Vars {
         if let Some(v) = t_bool(&mut table, "hmac_otp", "enable", "HOTP_ENABLE") {
             self.hmac_otp.enable = v;
         }
+        if let Some(v) = t_u8(&mut table, "hmac_otp", "length", "HOTP_LENGTH") {
+            self.hmac_otp.length = v;
+        }
     }
 
     fn parse_http_client(&mut self, table: &mut toml::Table) {
@@ -3918,6 +3922,7 @@ pub struct VarsHashing {
 #[derive(Debug)]
 pub struct VarsHOTP {
     pub enable: bool,
+    pub length: u8,
 }
 
 #[derive(Debug)]
